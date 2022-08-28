e7cc2f75f412e91a36c55b37e8a5c3de16248132
Ahmed0107 <65514906+Ahmed0107@users.noreply.github.com>
2022-08-28 16:15:58 +0200

    adding a new files

---
+++ app.js
@@ -0,0 +1,20 @@
+var res = prompt("enter your name");
+
+
+switch(res){
+    case "ahmed" :
+        document.write("Hi " + res)
+    break;
+
+    case "ali" :
+        document.write("Hi " + res)
+    break;
+
+    case "osos" :
+        document.write("Hi " + res)
+    break;
+
+    default:
+        document.write("no " + res)
+    break;
+}
