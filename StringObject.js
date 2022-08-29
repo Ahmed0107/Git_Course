48c02cf47e35b1f661af6b1b05c87820fdc6b86f
Ahmed0107 <65514906+Ahmed0107@users.noreply.github.com>
2022-08-29 15:04:05 +0200

    StringObject.js files

---
+++ StringObject.js
@@ -0,0 +1,16 @@
+// String Oblect Methods and Properties
+
+// How Can We Create a String in JS
+
+// 1- Using leteral creation witch return a string (the type of creation will be a string)
+var myStr = "Welcome to JavaScript"
+
+// 2- Using Constructot creation witch return a object (the type of creation will be a object)
+var newStr = new String("A JavaScript String Object")
+
+// 1- Using String function creation witch return a string (the type of creation will be a string)
+var str = "A JavaScript string"
+
+console.log(typeof myStr);
+console.log(typeof newStr);
+console.log(typeof str);
